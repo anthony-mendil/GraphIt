2a07379c2f44be2f02a340a1a55ebecba3675467
jjaeger <jjaeger@uni-bremen.de>
2018-12-04 11:38:52 +0100

    javadoc maven not yet working

---
+++ softwareprojekt2/src/main/java/JavaDocTestClass.java
@@ -0,0 +1,11 @@
+public class JavaDocTestClass {
+
+    /***
+     * Main
+     * @param args
+     */
+    public static void main (String args[]){
+        System.out.println("HelloWorld");
+    }
+
+}
