05fe9c9b397ae395e3b7c1692fe5d2e074f62e45
jjaeger <jjaeger@uni-bremen.de>
2018-12-07 13:06:13 +0100

    marcels version

--- softwareprojekt2/src/main/java/graph/graph/Sphaere.java
+++ softwareprojekt2/src/main/java/graph/graph/Sphaere.java
@@ -1,83 +1,26 @@
 package graph.graph;
 
-import edu.uci.ics.jung.visualization.VisualizationViewer;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.Value;
 
 import java.awt.*;
 
+@Data
 public class Sphaere {
+
+    @NonNull
     private Color color;
+
     private double xCoor;
     private double yCoor;
     private double width;
     private double height;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private Color fil;
     private final int id;
-
-
-    /**
-     * TODO
-     */
-    Sphaere(double xCoor, double yCoor, double width, double height, String name, int id){
-        throw new UnsupportedOperationException();
-    }
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public double getxCoor() {
-        return xCoor;
-    }
-
-    public void setxCoor(double xCoor) {
-        this.xCoor = xCoor;
-    }
-
-    public double getyCoor() {
-        return yCoor;
-    }
-
-    public void setyCoor(double yCoor) {
-        this.yCoor = yCoor;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Color getFil() {
-        return fil;
-    }
-
-    public void setFil(Color fil) {
-        this.fil = fil;
-    }
-
-    public int getId() {
-        return id;
-    }
 }
