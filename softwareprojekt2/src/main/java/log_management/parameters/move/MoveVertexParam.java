47a33107ac5f91ca843151af5a5ae3d1e8b0d158
Anthony Mendil <antmen@uni-bremen.de>
2018-12-09 00:31:33 +0100

    Alle Attribute ergänzt.

--- softwareprojekt2/src/main/java/log_management/parameters/move/MoveVertexParam.java
+++ softwareprojekt2/src/main/java/log_management/parameters/move/MoveVertexParam.java
@@ -6,8 +6,27 @@ import java.io.Serializable;
 
 public class MoveVertexParam extends Param implements Serializable {
 
-    // later add other attributes
-    public MoveVertexParam() {
-    }
+    private int vertexId;
+    private String vertexAnnotation;
+    private int oldSphereId;
+    private String oldSphereAnnotation;
+    private int newSphereId;
+    private String newSphereAnnotation;
+    private double oldXCoor;
+    private double newXCoor;
+    private double oldYCoor;
+    private double newYCoor;
 
+    public MoveVertexParam(int vertexId, String vertexAnnotation, int oldSphereId, String oldSphereAnnotation, int newSphereId, String newSphereAnnotation, double oldXCoor, double newXCoor, double oldYCoor, double newYCoor) {
+        this.vertexId = vertexId;
+        this.vertexAnnotation = vertexAnnotation;
+        this.oldSphereId = oldSphereId;
+        this.oldSphereAnnotation = oldSphereAnnotation;
+        this.newSphereId = newSphereId;
+        this.newSphereAnnotation = newSphereAnnotation;
+        this.oldXCoor = oldXCoor;
+        this.newXCoor = newXCoor;
+        this.oldYCoor = oldYCoor;
+        this.newYCoor = newYCoor;
+    }
 }
--- softwareprojekt2/src/main/java/log_management/parameters/move/MoveVerticesParam.java
+++ softwareprojekt2/src/main/java/log_management/parameters/move/MoveVerticesParam.java
@@ -3,11 +3,31 @@ package log_management.parameters.move;
 import LogManagement.Parameters.Param;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class MoveVerticesParam extends Param implements Serializable {
 
-    // later add other attributes
-    public MoveVerticesParam() {
-    }
+    private List<Integer> verticesId;
+    private List<String> verticesAnnotation;
+    private int oldSphereId;
+    private String oldSphereAnnotation;
+    private int newSphereId;
+    private String newSphereAnnotation;
+    private List<Double> oldXCoors;
+    private List<Double> newXCoors;
+    private List<Double> oldYCoors;
+    private List<Double> newYCoors;
 
+    public MoveVerticesParam(List<Integer> verticesId, List<String> verticesAnnotation, int oldSphereId, String oldSphereAnnotation, int newSphereId, String newSphereAnnotation, List<Double> oldXCoors, List<Double> newXCoors, List<Double> oldYCoors, List<Double> newYCoors) {
+        this.verticesId = verticesId;
+        this.verticesAnnotation = verticesAnnotation;
+        this.oldSphereId = oldSphereId;
+        this.oldSphereAnnotation = oldSphereAnnotation;
+        this.newSphereId = newSphereId;
+        this.newSphereAnnotation = newSphereAnnotation;
+        this.oldXCoors = oldXCoors;
+        this.newXCoors = newXCoors;
+        this.oldYCoors = oldYCoors;
+        this.newYCoors = newYCoors;
+    }
 }
