877b7035ae42d68e15cd774e3c85f9c86636ac46
machja <machja@uni-bremen.de>
2018-12-11 21:06:34 +0100

    Controller finished

--- softwareprojekt2/src/main/java/gui/Controller.java
+++ softwareprojekt2/src/main/java/gui/Controller.java
@@ -6,202 +6,326 @@ public class Controller {
 
     /* ----------------ACTIVATE---------------------- */
 
+    /**
+     *  Executes the activateAnchorPointsFadeout-Action
+     */
     public void activateAnchorPointsFadeOut(){
         throw new NotImplementedException();
     }
 
-    public void activateFadeOut(){
+    /**
+     *  Executes the activateFadeoutLog-Action
+     */
+    public void activateFadeout(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the highlightLog-Action
+     */
     public void activateHighlight(){
         throw new NotImplementedException();
     }
 
     /* ----------------ADD---------------------- */
 
-    /* nur über Drop Down Menü */
-    public void addAnchorPoints(){
-        throw new NotImplementedException();
-    }
-
+    /**
+     *  Executes the addSphereLog-Action
+     */
     public void addSphere(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the addVertexLog-Action
+     */
     public void addVertex(){
         throw new NotImplementedException();
     }
 
     /* ----------------ANALYSE---------------------- */
 
+    /**
+     *  Executes the analysisGraph-Action
+     */
     public void analysisGraph(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the evaluationGraph-Action
+     */
     public void evaluationGraph(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the filterGraph-Action
+     */
     public void filterGraph(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the graphDimension-Action
+     */
     public void graphDimension(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the selectionAnalysisGraph-Action
+     */
     public void selectionAnalysisGraph(){
         throw new NotImplementedException();
     }
 
     /* ----------------DEACTIVATE---------------------- */
 
+    /**
+     *  Executes the deactivateAnchorPointsFadeout-Action
+     */
     public void deactivateAnchorPointsFadeout(){
         throw new NotImplementedException();
     }
 
     /* nur über Drop-Down-Menü */
+    /**
+     *  Executes the deactivateFadeout-Action
+     */
     public void deactivateFadeout(){
         throw new NotImplementedException();
     }
 
     /* nur über Drop-Down-Menü */
+    /**
+     *  Executes the deactivateHighlight-Action
+     */
     public void deactivateHighlight(){
         throw new NotImplementedException();
     }
 
     /* ----------------EDIT---------------------- */
 
+    /**
+     *  Executes the editEdgesStroke-Action
+     */
     public void editEdgesStroke(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editEdgesType-Action
+     */
     public void editEdgesType(){
         throw new NotImplementedException();
     }
 
     /* ......annotation..... */
 
+    /**
+     *  Executes the editSphereAnnotation-Action
+     */
     public void editSphereAnnotation(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editVertexAnnotation-Action
+     */
     public void editVertexAnnotation(){
         throw new NotImplementedException();
     }
 
     /* ......color..... */
 
+    /**
+     *  Execute the editEdgeColor-Action
+     */
     public void editEdgeColor(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Execute the editEdgesColor-Action
+     */
     public void editEdgesColor(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editSphereColor-Action
+     */
     public void editSphereColor(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editVertexColor-Action
+     */
     public void editVertexColor(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editVerticesColor-Action
+     */
     public void editVerticesColor(){
         throw new NotImplementedException();
     }
 
     /* ......font..... */
 
+    /**
+     *  Executes the editFont-Action
+     */
     public void editFont(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editFontSize-Action
+     */
     public void editFontSize(){
         throw new NotImplementedException();
     }
 
     /* ......form..... */
 
+    /**
+     *  Executes the editVertexForm-Action
+     */
     public void editVertexForm(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editVerticesForm-Action
+     */
     public void editVerticesForm(){
         throw new NotImplementedException();
     }
 
     /* ......size..... */
 
+    /**
+     *  Executes the editSphereSize-Action
+     */
     public void editSphereSize(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the editVertexSize-Action
+     */
     public void editVertexSize(){
         throw new NotImplementedException();
     }
 
     /* ----------------EXPORT---------------------- */
 
+    /**
+     *  Executes the exportGXL-Action
+     */
     public void exportGXL(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the exportPDF-Action
+     */
     public void exportPDF(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the exportOOF-Action
+     */
+    public void exportOOF(){
+        throw new NotImplementedException();
+    }
+
+    /**
+     *  Executes the importOOF-Action
+     */
+    public void importOOF(){
+        throw new NotImplementedException();
+    }
+
+    /**
+     *  Executes the importGXL-Action
+     */
     public void importGXL(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the printPDF-Action
+     */
     public void printPDF(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the exportTemplate-Action
+     */
     public void exportTemplate(){
         throw new NotImplementedException();
     }
 
     /* ----------------LAYOUT---------------------- */
 
+    /**
+     *  Executes the layoutGraph-Action
+     */
     public void layoutGraph(){
         throw new NotImplementedException();
     }
 
     /* ----------------OTHER---------------------- */
 
+    /**
+     *  Executes the changeLanguage-Action
+     */
     public void changeLanguage(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the switchModiEditor-Action
+     */
     public void switchModiEditor(){
         throw new NotImplementedException();
     }
 
     /* ----------------REMOVE---------------------- */
 
-    //Nur über Drop-Down-Menü
-    public void removeAnchorPoints(){
-        throw new NotImplementedException();
-    }
-
+    /**
+     *  Executes the removeEdges-Action
+     */
     public void removeEdges(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the removeSphere-Action
+     */
     public void removeSphere(){
         throw new NotImplementedException();
     }
 
+    /**
+     *  Executes the removeVertices-Action
+     */
     public void removeVertices(){
         throw new NotImplementedException();
     }
 
     /* ----------------TEMPLATE---------------------- */
 
+    /**
+     *  Executes the rulesTemplate-Action
+     */
     public void rulesTemplate(){
         throw new NotImplementedException();
     }
