0e5859c490b448947b7994e3daaeca1ecb5c35f3
jjaeger <jjaeger@uni-bremen.de>
2018-12-11 14:33:21 +0100

    pdfhandler changes

--- softwareprojekt2/src/main/java/actions/export_graph/PDFHandler.java
+++ softwareprojekt2/src/main/java/actions/export_graph/PDFHandler.java
@@ -1,5 +1,43 @@
 package actions.export_graph;
 
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import org.freehep.graphicsbase.util.export.ExportDialog;
+
 public class PDFHandler {
-    
+
+    private VisualizationViewer vv;
+
+    /**
+     * Constructs a new PDFHandler of the the current graph visualization.
+     *
+     * @param pVv The VisualizationViewer of the current graph
+     * @param pName The name of the Syndrom, that will become the filename
+     */
+    public PDFHandler(VisualizationViewer pVv, String pName){
+        vv=pVv;
+    }
+
+    /**
+     * Creates a PDF of the current graph visualization
+     * TODO: PDF AS RETURN VALUE?
+     */
+    private void createPDF(){
+
+    }
+
+    /**
+     * Starts the dialog to export the current graph visualization as PDF
+     * TODO: JUST AS PDF?
+     */
+    public void exportPDF(){
+        ExportDialog export = new ExportDialog();
+        export.showExportDialog(vv, "Export view as ...", vv, "export");
+    }
+
+    /**
+     * Starts the dialog to export the current graph visualization as PDF
+     */
+    public void printPDF(){
+
+    }
 }
