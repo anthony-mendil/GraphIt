0b2cc75641f65a573ac46980cc9bdde7358554db
jjaeger <jjaeger@uni-bremen.de>
2018-12-11 18:04:39 +0100

    GXLHandler und PDFAction changed

--- softwareprojekt2/src/main/java/actions/export_graph/ExportPdfAction.java
+++ softwareprojekt2/src/main/java/actions/export_graph/ExportPdfAction.java
@@ -1,7 +1,23 @@
 package actions.export_graph;
 
+import actions.GraphAction;
+
 /*
     Aktionsleiste -> Datei.. -> Exportieren als.. -> PDF
  */
-public class ExportPdfAction {
+public class ExportPdfAction extends GraphAction {
+
+    public ExportPdfAction(){
+
+    }
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
 }
--- softwareprojekt2/src/main/java/actions/export_graph/GXLHandler.java
+++ softwareprojekt2/src/main/java/actions/export_graph/GXLHandler.java
@@ -6,32 +6,40 @@ import graph.graph.Vertex;
 
 public class GXLHandler {
 
-    private Layout<Vertex, Edge> layout;
-    private String name;
 
     /**
-     *
-     *
-     * @param pLayout
-     * @param pName
+     * Constructs a new GXLHandler
      */
-    public GXLHandler(Layout<Vertex, Edge> pLayout, String pName){
-        layout=pLayout;
-        name=pName;
+    public GXLHandler(){
     }
 
     /**
+     * Creates a GXL instance of the current existing Graph as String
      *
+     * @param pLayout The layout representation of the current graph
+     *
+     * @return The current GXL instance as String
      */
-    public void exportGXL(){
-
+    public String createGXLString(Layout<Vertex,Edge> pLayout){
+        throw new UnsupportedOperationException();
     }
 
     /**
+     * Opens a dialog to save the the GXL representation to a specific location
      *
+     * @param pName The name of the Syndrom and the default filename
      */
-    public void importGXL(){
+    public void exportGXL(Layout<Vertex,Edge> pLayout, String pName){
+        //createGXL(pLayout) to File with pName as default filename
+    }
 
+    /**
+     * Opens a dialog to import a GXL file from a specific location to a layout representation of the current graph
+     *
+     * @return The layout representation of the graph
+     */
+    public Layout<Vertex, Edge> importGXL(){
+        throw new UnsupportedOperationException();
     }
 
 }
