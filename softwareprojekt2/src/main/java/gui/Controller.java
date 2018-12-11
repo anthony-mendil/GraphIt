aa09c2931f12e3b25570f5a6579790657592e7be
machja <machja@uni-bremen.de>
2018-12-11 18:08:55 +0100

    Controller, Main

---
+++ softwareprojekt2/src/main/java/gui/Controller.java
@@ -0,0 +1,215 @@
+package gui;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Controller {
+
+    /* ----------------ACTIVATE---------------------- */
+
+    public void activateAnchorPointsFadeOut(){
+        throw new NotImplementedException();
+    }
+
+    public void activateFadeOut(){
+        throw new NotImplementedException();
+    }
+
+    public void activateHighlight(){
+        throw new NotImplementedException();
+    }
+
+    /* ----------------ADD---------------------- */
+
+    /* nur über Drop Down Menü */
+    public void addAnchorPoints(){
+    }
+
+    public void addSphere(){
+
+    }
+
+    public void addVertex(){
+
+    }
+
+    /* ----------------ANALYSE---------------------- */
+
+    public void analysisGraph(){
+
+    }
+
+    public void evaluationGraph(){
+
+    }
+
+    public void filterGraph(){
+
+    }
+
+    public void graphDimension(){
+
+    }
+
+    public void selectionAnalysisGraph(){
+
+    }
+
+    /* ----------------DEACTIVATE---------------------- */
+
+    public void deactivateAnchorPointsFadeout{
+
+    }
+
+    public void deactivateFadeout{
+
+    }
+
+    public void deactivateHighlight{
+
+    }
+
+    /* ----------------EDIT---------------------- */
+
+    public void editEdgesStroke(){
+
+    }
+
+    public void editEdgesType(){
+
+    }
+
+    /* ......annotation..... */
+
+    public void changeVertexAnnotationLanguage(){
+
+    }
+
+    public void editSphereAnnotation(){
+
+    }
+
+    public void editVertexAnnotation(){
+
+    }
+
+    /* ......color..... */
+
+    public void editEdgeColor(){
+
+    }
+
+    public void editEdgesColor(){
+
+    }
+
+    public void editSphereColor(){
+
+    }
+
+    public void editVertexColor(){
+
+    }
+
+    public void editVerticesColor(){
+
+    }
+
+    /* ......font..... */
+
+    public void editFont(){
+
+    }
+
+    public void editFontSize(){
+
+    }
+
+    /* ......form..... */
+
+    public void editVertexForm(){
+
+    }
+
+    public void editVerticesForm(){
+
+    }
+
+    /* ......size..... */
+
+    public void editSphereSize(){
+
+    }
+
+    public void editVertexSize(){
+
+    }
+
+    /* ----------------EXPORT---------------------- */
+
+    public void exportGXL(){
+
+    }
+
+    public void exportPDF(){
+
+    }
+
+    public void importGXL(){
+
+    }
+
+    public void printPDF(){
+
+    }
+
+    /* ----------------LAYOUT---------------------- */
+
+    public void layoutGraph(){
+
+    }
+
+    /* ----------------MOVE---------------------- */
+
+    public void moveSphere(){
+
+    }
+
+    public void moveVertices(){
+
+    }
+
+    /* ----------------OTHER---------------------- */
+
+    public void changeLanguage(){
+
+    }
+
+    public void switchModiEditor(){
+
+    }
+
+    /* ----------------REMOVE---------------------- */
+
+    public void removeAnchorPoints(){
+
+    }
+
+    public void removeEdges(){
+
+    }
+
+    public void removeSphere(){
+
+    }
+
+    public void removeVertices(){
+
+    }
+
+    /* ----------------TEMPLATE---------------------- */
+
+    public void rulesTemplate(){
+
+    }
+
+}
