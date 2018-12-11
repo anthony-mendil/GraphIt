8c049deeba6c7b169e9aa5a56196499ab11fbd6f
machja <machja@uni-bremen.de>
2018-12-11 18:40:36 +0100

    Controller-Methoden, Name Change

--- softwareprojekt2/src/main/java/actions/edit/annotation/ChangeObjectAnnotationLanguageAction.java
+++ softwareprojekt2/src/main/java/actions/edit/annotation/ChangeObjectAnnotationLanguageAction.java
@@ -5,5 +5,5 @@ package actions.edit.annotation;
     Aktionsleiste -> Optionen -> Sprache ändern.. -> Englisch
     Aktionsleiste -> Optionen -> Sprache ändern.. -> <andere Sprachen>
  */
-public class ChangeVertexAnnotationLanguageAction {
+public class ChangeObjectAnnotationLanguageAction {
 }
--- softwareprojekt2/src/main/java/gui/Controller.java
+++ softwareprojekt2/src/main/java/gui/Controller.java
@@ -22,194 +22,188 @@ public class Controller {
 
     /* nur über Drop Down Menü */
     public void addAnchorPoints(){
+        throw new NotImplementedException();
     }
 
     public void addSphere(){
-
+        throw new NotImplementedException();
     }
 
     public void addVertex(){
-
+        throw new NotImplementedException();
     }
 
     /* ----------------ANALYSE---------------------- */
 
     public void analysisGraph(){
-
+        throw new NotImplementedException();
     }
 
     public void evaluationGraph(){
-
+        throw new NotImplementedException();
     }
 
     public void filterGraph(){
-
+        throw new NotImplementedException();
     }
 
     public void graphDimension(){
-
+        throw new NotImplementedException();
     }
 
     public void selectionAnalysisGraph(){
-
+        throw new NotImplementedException();
     }
 
     /* ----------------DEACTIVATE---------------------- */
 
-    public void deactivateAnchorPointsFadeout{
-
+    public void deactivateAnchorPointsFadeout(){
+        throw new NotImplementedException();
     }
 
-    public void deactivateFadeout{
-
+    /* nur über Drop-Down-Menü */
+    public void deactivateFadeout(){
+        throw new NotImplementedException();
     }
 
-    public void deactivateHighlight{
-
+    /* nur über Drop-Down-Menü */
+    public void deactivateHighlight(){
+        throw new NotImplementedException();
     }
 
     /* ----------------EDIT---------------------- */
 
     public void editEdgesStroke(){
-
+        throw new NotImplementedException();
     }
 
     public void editEdgesType(){
-
+        throw new NotImplementedException();
     }
 
     /* ......annotation..... */
 
-    public void changeVertexAnnotationLanguage(){
-
-    }
-
     public void editSphereAnnotation(){
-
+        throw new NotImplementedException();
     }
 
     public void editVertexAnnotation(){
-
+        throw new NotImplementedException();
     }
 
     /* ......color..... */
 
     public void editEdgeColor(){
-
+        throw new NotImplementedException();
     }
 
     public void editEdgesColor(){
-
+        throw new NotImplementedException();
     }
 
     public void editSphereColor(){
-
+        throw new NotImplementedException();
     }
 
     public void editVertexColor(){
-
+        throw new NotImplementedException();
     }
 
     public void editVerticesColor(){
-
+        throw new NotImplementedException();
     }
 
     /* ......font..... */
 
     public void editFont(){
-
+        throw new NotImplementedException();
     }
 
     public void editFontSize(){
-
+        throw new NotImplementedException();
     }
 
     /* ......form..... */
 
     public void editVertexForm(){
-
+        throw new NotImplementedException();
     }
 
     public void editVerticesForm(){
-
+        throw new NotImplementedException();
     }
 
     /* ......size..... */
 
     public void editSphereSize(){
-
+        throw new NotImplementedException();
     }
 
     public void editVertexSize(){
-
+        throw new NotImplementedException();
     }
 
     /* ----------------EXPORT---------------------- */
 
     public void exportGXL(){
-
+        throw new NotImplementedException();
     }
 
     public void exportPDF(){
-
+        throw new NotImplementedException();
     }
 
     public void importGXL(){
-
+        throw new NotImplementedException();
     }
 
     public void printPDF(){
+        throw new NotImplementedException();
+    }
 
+    public void exportTemplate(){
+        throw new NotImplementedException();
     }
 
     /* ----------------LAYOUT---------------------- */
 
     public void layoutGraph(){
-
-    }
-
-    /* ----------------MOVE---------------------- */
-
-    public void moveSphere(){
-
-    }
-
-    public void moveVertices(){
-
+        throw new NotImplementedException();
     }
 
     /* ----------------OTHER---------------------- */
 
     public void changeLanguage(){
-
+        throw new NotImplementedException();
     }
 
     public void switchModiEditor(){
-
+        throw new NotImplementedException();
     }
 
     /* ----------------REMOVE---------------------- */
 
+    //Nur über Drop-Down-Menü
     public void removeAnchorPoints(){
-
+        throw new NotImplementedException();
     }
 
     public void removeEdges(){
-
+        throw new NotImplementedException();
     }
 
     public void removeSphere(){
-
+        throw new NotImplementedException();
     }
 
     public void removeVertices(){
-
+        throw new NotImplementedException();
     }
 
     /* ----------------TEMPLATE---------------------- */
 
     public void rulesTemplate(){
-
+        throw new NotImplementedException();
     }
 
 }
