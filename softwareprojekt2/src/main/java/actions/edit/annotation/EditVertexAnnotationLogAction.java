d09560e2ec77ee359a59605a7309e265351d45e6
Clement <clement1@uni-bremen.de>
2019-01-21 19:26:18 +0100

    redo implemented and some minor bugfixes

--- softwareprojekt2/src/main/java/actions/edit/annotation/EditVertexAnnotationLogAction.java
+++ softwareprojekt2/src/main/java/actions/edit/annotation/EditVertexAnnotationLogAction.java
@@ -15,7 +15,6 @@ import java.util.Map;
  * Changes the annotation of a selected Vertex.
  */
 public class EditVertexAnnotationLogAction extends LogAction {
-    private String text;
     /**
      * The annotation of the vertex.
      */
