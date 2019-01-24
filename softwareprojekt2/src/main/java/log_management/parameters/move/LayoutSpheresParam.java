47d8c42acab8e093a5bb25373e4c8786981cd590
Anthony Mendil <antmen@uni-bremen.de>
2019-01-24 19:25:30 +0100

    fix für philip

--- softwareprojekt2/src/main/java/log_management/parameters/move/LayoutSpheresParam.java
+++ softwareprojekt2/src/main/java/log_management/parameters/move/LayoutSpheresParam.java
@@ -12,12 +12,27 @@ import java.io.Serializable;
 import java.util.Map;
 
 public class LayoutSpheresParam extends Param implements Serializable {
+    /**
+     * The vertices containing their old position.
+     */
+    @Getter
+    private Map<Sphere, Point2D> oldSpheres;
+
+    /**
+     * The map from vertex ids to the new position.
+     */
+    @Getter
+    private Map<Sphere, Point2D> newSpheres;
 
     /**
      * Creates a vertices object of its own class.
+     *
+     * @param pOldPosition Map of vertices containing their old positions.
+     * @param pNewPosition Map of vertices containing their new positions.
      */
-    public LayoutSpheresParam() {
-
+    public LayoutSpheresParam(Map<Sphere, Point2D> pOldPosition, Map<Sphere, Point2D> pNewPosition) {
+        this.oldSpheres = pOldPosition;
+        this.newSpheres= pNewPosition;
     }
     @Override
     public String toString() {
--- softwareprojekt2/src/main/java/log_management/parameters/move/LayoutVerticesParam.java
+++ softwareprojekt2/src/main/java/log_management/parameters/move/LayoutVerticesParam.java
@@ -17,13 +17,28 @@ import java.util.Map;
  */
 @Data
 public class LayoutVerticesParam extends Param implements Serializable {
+    /**
+     * The vertices containing their old position.
+     */
+    @Getter
+    private Map<Vertex, Point2D> oldVertices;
+
+    /**
+     * The map from vertex ids to the new position.
+     */
+    @Getter
+    private Map<Vertex, Point2D> newVertices;
 
     /**
      * Creates a vertices object of its own class.
      *
+     * @param pOldPosition Map of vertices containing their old positions.
+     * @param pNewPosition Map of vertices containing their new positions.
      */
-    public LayoutVerticesParam() {}
-
+    public LayoutVerticesParam(Map<Vertex, Point2D> pOldPosition, Map<Vertex, Point2D> pNewPosition) {
+        this.oldVertices = pOldPosition;
+        this.newVertices = pNewPosition;
+    }
     @Override
     public String toString() {
         Language language = Values.getInstance().getGuiLanguage();
