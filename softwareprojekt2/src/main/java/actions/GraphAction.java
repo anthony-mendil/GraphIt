b7dc226caf1aec42853524abb109bf682e868a3b
Clement <clement1@uni-bremen.de>
2019-02-11 12:43:51 +0100

    General Template rules has been implemented.

--- softwareprojekt2/src/main/java/actions/GraphAction.java
+++ softwareprojekt2/src/main/java/actions/GraphAction.java
@@ -1,6 +1,7 @@
 package actions;
 
 import graph.graph.Syndrom;
+import graph.graph.Template;
 import graph.visualization.control.HelperFunctions;
 import gui.Values;
 
@@ -14,11 +15,18 @@ public abstract class GraphAction extends Action {
      * The syndrom to work with.
      */
     protected Syndrom syndrom = Syndrom.getInstance();
-
+    /**
+     * The values to get the data from.
+     */
     protected Values values = Values.getInstance();
-
+    /**
+     * Helper-functions helping with the alert-text.
+     */
     protected HelperFunctions helper = new HelperFunctions();
-
+    /**
+     * The template returning information about the existing rules.
+     */
+    protected Template template = Template.getInstance();
     /**
      * Sets layout, graph, pick support from the current syndrom presentation.
      */
