0d49e4f2dee8cb9a905f5dfd845ba99f0b0d96a6
jjaeger <jjaeger@uni-bremen.de>
2019-02-26 11:29:57 +0100

    preloader

---
+++ softwareprojekt2/src/main/java/gui/FirstPreloader.java
@@ -0,0 +1,43 @@
+package gui;
+
+import javafx.application.Preloader;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class FirstPreloader extends Preloader {
+
+    ProgressBar bar;
+    Stage stage;
+
+    private Scene createPreloaderScene() {
+        bar = new ProgressBar();
+        BorderPane p = new BorderPane();
+        p.setCenter(bar);
+        Scene sz=new Scene(p, 300, 150);
+        return sz;
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.initStyle(StageStyle.UNDECORATED);
+        this.stage = stage;
+        stage.setScene(createPreloaderScene());
+        stage.setAlwaysOnTop(true);
+        stage.show();
+    }
+
+    @Override
+    public void handleProgressNotification(ProgressNotification pn) {
+        bar.setProgress(pn.getProgress());
+    }
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification evt) {
+        if (evt.getType() == StateChangeNotification.Type.BEFORE_START) {
+            stage.hide();
+        }
+    }
+}
