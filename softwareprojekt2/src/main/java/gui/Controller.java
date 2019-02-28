f603ae37ced59bd33f68e0bb57390b159abfda8d
nnina <nin_unt@uni-bremen.de>
2019-02-28 10:51:31 +0100

    fix picked

--- softwareprojekt2/src/main/java/gui/Controller.java
+++ softwareprojekt2/src/main/java/gui/Controller.java
@@ -1014,7 +1014,7 @@ public class Controller implements ObserverSyndrom {
      * Creates an EditEdgesStrokeLogAction-object and executes the action with the action history.
      */
     private void editEdgesStroke(StrokeType stroke) {
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedEdgeState().getPicked().isEmpty()) {
             EditEdgesStrokeLogAction editEdgesStrokeLogAction = new EditEdgesStrokeLogAction(stroke);
             history.execute(editEdgesStrokeLogAction);
         }
@@ -1054,7 +1054,7 @@ public class Controller implements ObserverSyndrom {
      * Creates an EditEdgesTypeLogAction-object and executes the action with the action history.
      */
     private void editEdgesType(EdgeArrowType type) {
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedEdgeState().getPicked().isEmpty()) {
             EditEdgesTypeLogAction editEdgesTypeLogAction = new EditEdgesTypeLogAction(type);
             history.execute(editEdgesTypeLogAction);
         }
@@ -1101,7 +1101,7 @@ public class Controller implements ObserverSyndrom {
     public void editEdgesColor() {
         Color color = convertToAWT(edgeColour.getValue());
         Values.getInstance().setEdgePaint(color);
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedEdgeState().getPicked().isEmpty()) {
             EditEdgesColorLogAction editEdgesColorLogAction = new EditEdgesColorLogAction(color);
             history.execute(editEdgesColorLogAction);
         }
@@ -1128,7 +1128,7 @@ public class Controller implements ObserverSyndrom {
     public void editSphereColor() {
         Color color = convertToAWT(sphereBackgroundColour.getValue());
         Values.getInstance().setFillPaintSphere(color);
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedSphereState().getPicked().isEmpty()) {
             EditSphereColorLogAction colorLogAction = new EditSphereColorLogAction(color);
             history.execute(colorLogAction);
         }
@@ -1183,7 +1183,7 @@ public class Controller implements ObserverSyndrom {
      */
     private void editFontSphere(String font) {
         values.setFontSphere(font);
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedSphereState().getPicked().isEmpty()) {
             EditFontSphereLogAction editFontSphereLogAction = new EditFontSphereLogAction(font);
             history.execute(editFontSphereLogAction);
         }
@@ -2083,7 +2083,7 @@ public class Controller implements ObserverSyndrom {
 
     public void editFontSizeSphere(int size) {
         values.setFontSizeSphere(size);
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedSphereState().getPicked().isEmpty()) {
             EditFontSizeSphereLogAction editFontSizeSphereLogAction = new EditFontSizeSphereLogAction(size);
             history.execute(editFontSizeSphereLogAction);
         }
@@ -2719,14 +2719,14 @@ public class Controller implements ObserverSyndrom {
     }
 
     public void sphereEnlarge() {
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedSphereState().getPicked().isEmpty()) {
             EditSphereSizeLogAction editSphereSizeLogAction = new EditSphereSizeLogAction(SizeChange.ENLARGE);
             history.execute(editSphereSizeLogAction);
         }
     }
 
     public void sphereShrink() {
-        if (!syndrom.getVv().getPickedVertexState().getPicked().isEmpty()) {
+        if (!syndrom.getVv().getPickedSphereState().getPicked().isEmpty()) {
             EditSphereSizeLogAction editSphereSizeLogAction = new EditSphereSizeLogAction(SizeChange.SHRINK);
             history.execute(editSphereSizeLogAction);
         }
